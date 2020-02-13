bdfb393714b6cc6bfbb54f79bf6dd3180efdd964
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-13 19:11:30 +0530

    [Ajay]Add.implementation of Usecase_5_monthly_wage

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,2 +1,13 @@
-echo welcome to employe wage computation problem
+echo " calculate total income of employee in a month"
+days=0
+for((i=1;i<=20;i++))
+do
+a=$((RANDOM%2))
+if(( $a==0 ))
+then
+days=$(($days+1))
+fi
+done
+echo "total part time work $days"
+
 
