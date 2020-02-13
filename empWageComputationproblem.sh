f34720f8cfb2c96ad6e0ed477e9e14212e7a9134
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-13 17:55:18 +0530

    [Ajay]Add.modified

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,2 +1,12 @@
-echo welcome to employe wage computation problem
+echo "select  choice who you want to execute"
+echo " 1 -check attendence"
+echo "2-check Daily employe wage"
+echo "3-part_time_wage"
+read input
+case $input in 
+1)
+echo employe present $present day in 20 days
+;;
+2)
+CalculateDailyEm
 
