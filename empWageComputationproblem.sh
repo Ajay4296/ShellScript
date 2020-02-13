3f453c445e6cb2343bb08c01da7d1ef4245d868d
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-13 12:36:43 +0530

    [ajay]Add.adding useCase-2 branch with function for dailywage

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,2 +1,7 @@
-echo welcome to employe wage computation problem
-
+function dailywage()
+{
+	hours_per_day=8
+	wage_per_hour=20
+	daily_wage=$(($hours_per_day * $wage_per_hour))
+	return $daily_wage
+{
