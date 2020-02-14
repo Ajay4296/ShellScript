b8b1f91fb4cf02547388ade71d48cf2ce8a76f22
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-14 13:13:13 +0530

    [Ajay]Refactor.modification of UC-3

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,8 +1,9 @@
-echo "Add part time employe and wage"
-function part_time()
+echo "Add part time employe and wag
+PartTime()
 {
 perdayhour=4
 wage=20
 totalparttimewage=$(( $perdayhour*$wage ))
 return $totalparttimewage
+}
 
