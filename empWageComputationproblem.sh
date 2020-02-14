b9d88fc121594114e3bab8c58095d17ffff8a14e
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-14 11:40:02 +0530

    [Ajay]Add.implementation of UC-8

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,2 +1,9 @@
-echo welcome to employe wage computation problem
+DailyWage
+perdaydailywage=$?
+PartTime
+Parttimedailywage=$?
+totaldailywage=$(($perdaydailywage+$parttimedailywage))
+totalincome=$totalIncome
+echo "total daily income of the employe is=$totaldailywage"
+echo "total monthly income of employe is=$totalincome"
 
