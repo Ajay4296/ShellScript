2fe0bc07370a92d0163136baf10d961a4d93c322
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-17 17:42:04 +0530

    [Ajay]Refactor.improvment in develop complete code

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -102,6 +102,22 @@ PartTime
 parttimedailywage=$?
 totaldailywage=$(($perdaydailywage+$parttimedailywage))
 totalincome=$totalIncome
+workinghours=0
+dailyworking=0
+while [[ $workinghours -le 100 ]]
+do
+dailyworking=$(($dailyworking+$((8*20))))
+workinghours=$(($workinghours+8))
+if((workinghours==96))
+then
+workinghours=$(($workinghours+4))
+dailyworking=$(($dailyworking+$((4*20))))
+fi
+done
+echo  "dailyworking $dailyworking"
+
+
+
 echo "total work hour of employee in a month=$wh"
 echo "total daily income of the employe is=$totaldailywage"
 echo "total monthly income of employe is=$totalincome"
