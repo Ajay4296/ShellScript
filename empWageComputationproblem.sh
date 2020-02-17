fd4d64226bf06a3bc6e9938b1a191b14542ffda5
Abhishekrathore1994 <Ajayk4296@gmail.com>
2020-02-17 17:50:54 +0530

    [Ajay]Add. implementation of  UC-6_run loop untill 100 hour

--- empWageComputationproblem.sh
+++ empWageComputationproblem.sh
@@ -1,2 +1,16 @@
-echo welcome to employe wage computation problem
+workinghours=0
+dailyworking=0
+while [[ $workinghours -le 100 ]]
+do
+dailyworking=$(($dailyworking+$((8*20))))
+workinghours=$(($workinghours+8))
+if((workinghours==96))
+then
+workinghours=$(($workinghours+4))
+dailyworking=$(($dailyworking+$((4*20))))
+fi
+done
+echo  "dailyworking $dailyworking"
+
+
 
